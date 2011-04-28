ac9db97c4d1d409d699365fb5ba01bfc1d7b3e13
joshatjben <josh@j-ben.com>
2011-04-28 21:41:30 +0000

    stable working with dummy data

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = require('express').createServer();
 var db = require('./lib/muemenDB.js');
+var whiskers = require('whiskers');
 
 function getClientAddress(req) {
   return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -18,25 +19,26 @@ db.addPage(
     "jbennett",
     //TODO this should be passed the unique hash, based on, ip or host name, and hashed password (stored in session or cookie)?
     "index", 
-    '<html><head>{title}</head><body><h1>{title}</h1><p></p></body></html>',
+    '<html><head>{title}</head><body><h1>{title}</h1><p>{description}</p></body></html>',
     {
         title: "Hello World!",
-        description: "This is a test of the muemen system."
+        description: "This is a test of the muemen system.",
     }
 );
+
 //-----END OF DUMMY DATA
 
 function loadUser(req, res, next) {
   // You would fetch your user from the db
   var user = req.params.user;
-  var page = req.params.page;
+  var page = req.params.page || "index";
   var outpage = db.getPage(user, page);
   
   var outputMarkup = outpage.markup;
   var outputContext = outpage.context;
   
   if (user) {
-    req.generatedPage = outputMarkup;
+    req.output = {markup: outputMarkup, context:outputContext};
     next();
   } else {
     next(new Error('Failed to load user ' + req.params.user));
@@ -44,6 +46,10 @@ function loadUser(req, res, next) {
 }
 
 app.get('/:user/:page', loadUser, function(req, res){
+  res.send( whiskers.render(req.output.markup, req.output.context) );
+});
+
+app.get('/:user', loadUser, function(req, res){
   res.send(req.generatedPage);
 });
 
