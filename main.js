fdfd44f312dddd3d462a7f294999e7fc8c1be761
joshatjben <josh@j-ben.com>
2011-05-25 19:36:11 +0000

    update

--- main.js
+++ main.js
@@ -38,36 +38,34 @@ db.addPage(
     "jbennett",
     //TODO this should be passed the unique hash, based on, ip or host name, and hashed password (stored in session or cookie)?
     "index", 
-    '<html><head><title>{title}</title></head><body><h1>{title}</h1><p>{description}</p><ul>{for page in pages}<li><a href="{page.url}">{page.title}</a></li>{/for}</ul></body></html>',
+    '<html><head><title>{title}</title></head><body><h1>{title}</h1><p>{description}</p><ul>{for page in pages}<li><a href="/{page.url}">{page.title}</a></li>{/for}</ul></body></html>',
     {
         title: "Hello World!",
         description: "This is a test of the muemen system."
-    },
-    true
+    }
 );
 
 db.addPage(
     "jbennett",
     //TODO this should be passed the unique hash, based on, ip or host name, and hashed password (stored in session or cookie)?
     "subpage", 
-    '<html><head><title>{title}</title></head><body><h1>{title}</h1><p>{description}</p><ul>{for page in pages}<li><a href="{page.url}">{page.title}</a></li>{/for}</ul></body></html>',
+    '<html><head><title>{title}</title></head><body><h1>{title}</h1><p>{description}</p><ul>{for page in pages}<li><a href="/{page.url}">{page.title}</a></li>{/for}</ul></body></html>',
     {
         title: "A really awesome sub page",
         description: "This is how a sub page looks.<br /> although it's not really a sub page, since it's uber aweesoeme!"
-    },
-    true
+    }
 );
 
 db.addPage(
     "jbennett",
     //TODO this should be passed the unique hash, based on, ip or host name, and hashed password (stored in session or cookie)?
     "page1", 
-    '<html><head><title>{title}</title></head><body><h1>{title}</h1><p>{description}</p><ul>{for page in pages}<li><a href="{page.url}">{page.title}</a></li>{/for}</ul></body></html>',
+    '<html><head><title>{title}</title></head><body><h1>{title}</h1><p>{description}</p><ul>{for page in pages}<li><a href="/{page.url}">{page.title}</a></li>{/for}</ul></body></html>',
     {
         title: "Yup another page, aka Page1",
         description: "this page is another page labeld page #1"
     },
-    false
+    true
 );
 
 //-----END OF DUMMY DATA
